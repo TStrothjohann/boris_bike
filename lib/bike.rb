fb634fdedb16a825e5ebbe8ee34ca61b6ce41e0b
Vanessa <virgitti@gmail.com>
2015-02-03 14:35:41 +0000

    Complete Bike class description

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,19 @@
 class Bike
-	def broken?
-		#If the user tells us, the bike is broken -> return true 
-		#else false
+
+	def initialize
+		@broken = false
 	end
+		
+	
+	def broken? 
+	#If the user tells us, the bike is broken -> return true 
+	#else false
+		@broken
+
+	end
+
+	def break!
+		@broken = true
+	end
+
 end
