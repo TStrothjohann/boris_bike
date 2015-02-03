1c7f764aa9617c8fbfd836d20ce1dd6f1ce49d12
Vanessa <virgitti@gmail.com>
2015-02-03 19:14:17 +0000

    Added test for full station and release a bike

--- lib/station.rb
+++ lib/station.rb
@@ -1,6 +1,8 @@
 class Station
+	DEFAULT_CAPACITY = 10
 	
-	def initialize
+	def initialize(options = {})
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
 		@bikes = []
 	end
 
@@ -10,6 +12,14 @@ class Station
 
 	def dock(bike)
 		@bikes << bike
+	end
+
+	def release(bike)
+		@bikes.delete(bike)
+	end
+
+	def full?
+		bike_count == @capacity
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,24 @@ require 'station'
 
 describe Station do
 	let(:bike){Bike.new}
-	let(:station){Station.new}
+	let(:station){Station.new(capacity: 20)}
 
 	it 'should be able to accept bikes'  do 
 expect(station.bike_count).to eq(0)
 station.dock(bike)
 expect(station.bike_count).to eq(1)  
+	end
+
+	it 'should release a bike' do
+	station.dock(bike)
+	station.release(bike)
+	expect(station.bike_count).to eq(0)
+	end
+
+	it 'should know when it\'s full' do
+		expect(station).not_to be_full
+		20.times {station.dock(bike)}
+		expect(station).to be_full
 	end
 
 end
